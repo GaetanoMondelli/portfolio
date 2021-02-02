fd5c17e598a9dd204940b44984f8488637855d23
GaetanoMondelli <gaetano.mondelli@gmail.com>
2021-02-02 23:29:09 +0000

    feat:  added sketchy library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Book from './Book';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container" style={{ marginTop: "3%" }}>
+      <div class="jumbotron">
+        <h1 class="display-3">Benvenuto</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p class="lead">
+          </p>
+          <Book></Book>
+      </div>
+      </div>
+
   );
 }
 
